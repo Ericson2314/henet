d3a1123f9aa6d43f312240e8074515d7fea0ad27
John Ericson <Ericson2314@Yahoo.com>
2013-08-28 21:35:36 -0400

    updated version of BitSet, removed a few unused imports

--- src/Network/ENet/Packet.hs
+++ src/Network/ENet/Packet.hs
@@ -4,26 +4,20 @@ import Foreign
 import Foreign.C.Error
 import Foreign.C.Types
 
-import Data.Word
-
-import Data.BitSet.Generic(GBitSet, toBits, unsafeFromBits)
+import Data.BitSet.Generic(BitSet(getBits))
 
 import qualified Data.ByteString        as SB
 import qualified Data.ByteString.Unsafe as SB
 
-import Data.Endian
-import Data.Endian.Unsafe( unsafeUnwrapBigEndian
-                         , unsafeAssertBigEndian)
-
 import qualified Network.ENet.Bindings as B
 
-type PacketFlagSet = GBitSet Word32 B.PacketFlag
+type PacketFlagSet = BitSet Word32 B.PacketFlag
 
 -- String is defensively coppied, so unsafe is appropriate
 
 create :: SB.ByteString -> PacketFlagSet -> IO (Ptr B.Packet)
-create bytes flags = SB.unsafeUseAsCStringLen bytes $
-              \(ptr,len) -> B.packetCreate (castPtr ptr) (fromIntegral len) $ toBits flags
+create bytes flags = SB.unsafeUseAsCStringLen bytes $ \(ptr,len) ->
+  B.packetCreate (castPtr ptr) (fromIntegral len) $ getBits flags
 
 destroy :: Ptr B.Packet -> IO ()
 destroy = B.packetDestroy
